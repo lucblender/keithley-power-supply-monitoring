87e1a75f97480ae7ab82295af9e992830cd22b66
Lucas Bonvin <lucasbonvin@hotmail.com>
2025-02-08 18:21:46 +0100

    fix: add lock for data in voltage current buffer

--- voltage_current_buffer.py
+++ voltage_current_buffer.py
@@ -2,6 +2,7 @@
 
 import time
 from keithley_serial_api import KeithleySerialApi
+from threading import Lock
 
 
 class VoltageCurrentBuffer:
@@ -14,26 +15,29 @@ class VoltageCurrentBuffer:
         self.time_stamps_current = [[], [], []]
         self.__keithley_serial_api = __keithley_serial_api
         self.start_time = 0
+        self.data_lock = Lock()
 
     def update_data(self):
         """Update voltage and current readings."""
 
         for i in range(0, 3):
-            try:
-                self.data_voltage[i].append(
-                    float(self.__keithley_serial_api.get_voltage(i)))
-                self.time_stamps_voltage[i].append(
-                    time.time() - self.start_time)
-            except (TypeError, ValueError):
-                print("Voltage data couldn't be converted to float")
+            with self.data_lock:
+                try:
+                    self.data_voltage[i].append(
+                        float(self.__keithley_serial_api.get_voltage(i)))
+                    self.time_stamps_voltage[i].append(
+                        time.time() - self.start_time)
+                except (TypeError, ValueError):
+                    print("Voltage data couldn't be converted to float")
 
-            try:
-                self.data_current[i].append(
-                    float(self.__keithley_serial_api.get_current(i)))
-                self.time_stamps_current[i].append(
-                    time.time() - self.start_time)
-            except (TypeError, ValueError):
-                print("Current data couldn't be converted to float")
+            with self.data_lock:
+                try:
+                    self.data_current[i].append(
+                        float(self.__keithley_serial_api.get_current(i)))
+                    self.time_stamps_current[i].append(
+                        time.time() - self.start_time)
+                except (TypeError, ValueError):
+                    print("Current data couldn't be converted to float")
 
     def set_start_time_now(self):
         """set start_time to current time of the day"""
@@ -41,7 +45,9 @@ class VoltageCurrentBuffer:
 
     def clear_data(self):
         """clear all data"""
-        self.data_voltage = [[], [], []]
-        self.data_current = [[], [], []]
-        self.time_stamps_voltage = [[], [], []]
-        self.time_stamps_current = [[], [], []]
+
+        with self.data_lock:
+            self.data_voltage = [[], [], []]
+            self.time_stamps_voltage = [[], [], []]
+            self.data_current = [[], [], []]
+            self.time_stamps_current = [[], [], []]
